12d35581c85c2ad1896565e398aa8b8bbb2111fb
yuran1811 <trieuvanbd123@gmail.com>
2022-06-04 09:03:47 +0700

    gene the empty data

--- generate.js
+++ generate.js
@@ -6,6 +6,8 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const files = fs.readdirSync(__dirname);
 const htmlFiles = files.filter((_, idx) => extname(_) === '.html');
 
+const ids = Array(105).fill(0);
+
 const data = htmlFiles.map((_) => {
 	const filePath = resolve(__dirname, _);
 	const fileContent = fs.readFileSync(filePath, { encoding: 'utf8' });
@@ -28,11 +30,23 @@ const data = htmlFiles.map((_) => {
 
 	const { id, name } = getFileName(_);
 
+	ids[id] = 1;
+
 	return {
 		id,
 		name,
 		data: standardizeData(fileContent),
 	};
 });
+
+for (let i = 0; i <= 100; i++)
+	if (!ids[i])
+		data.push({
+			id: i,
+			name: '',
+			data: ``,
+		});
+
+console.log(data);
 
 fs.writeFileSync(resolve(__dirname, 'howToData.json'), JSON.stringify(data));
